7fc8717540a3765718a3ed1fea7d0140efccd9b8
lusia <truongmyhoa561@gmail.com>
2024-10-05 18:04:29 +0700

    Change about Clear and NumberandSign

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const stringsymbol = ["+", "-", "/", "x"];
 const stringnumber = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
 const arrayButton = [
@@ -22,6 +23,9 @@ const arrayButton = [
   ".",
   "=",
 ];
+
+const charClearAll = "C";
+const charClear ="CE";
 var number1 = "";
 var number2 = "";
 var symbol = "";
@@ -48,40 +52,79 @@ const displayNumberAndSign = (num) => {
 };
 
 const getValue = (id) => {
+
+  ////////// Clear C //////////////////
+  if(id == charClearAll)
+     {
+       displayNumber(0);
+       displayNumberAndSign("");
+       value = 0;
+       return;
+
+     }
   if (stringsymbol.includes(id)) {
    
 
     if (dau != 0) {
       let oldSymbol = symbol;
        symbol = id;
-       numberAndSign += " ";
-        numberAndSign += id + " ";
-      displayNumberAndSign(numberAndSign);
+       
+        numberAndSign += " " +  id + " ";
+ 
       
+     //////////////
+      const lastChar = numberAndSign[numberAndSign.length - 2];
+    const secondLastChar = numberAndSign[numberAndSign.length - 5]; 
+ 
+  
+     console.log({lastChar, secondLastChar});
+    // Kiểm tra xem cả hai ký tự đều là phép toán 
+    if (numberAndSign.length > 1 && stringsymbol.includes(lastChar) && stringsymbol.includes(secondLastChar)) {
+        // Xóa ký tự kế cuối
+        console.log("có vo dayy khong");
+         const secondLastCharIndex = numberAndSign.length - 5;
+       
+          numberAndSign = numberAndSign.slice(0, secondLastCharIndex) + numberAndSign.slice(secondLastCharIndex + 1);
+          console.log("giá lc") 
+        displayNumberAndSign(numberAndSign); // Cập nhật hiển thị
+    }
+
+          displayNumberAndSign(numberAndSign);
       if(number2 != ''){
+        
         let value = calculate(number1, number2, oldSymbol);
         number2 ='';
         number1 = value;
         displayNumber(value);
       }
-
+      
     }
   } else {
+
+    ///////////// CE ////////////////
+     if(id == charClear)
+      {
+        id = 0;
+        value = 0;
+      }
+
     if (symbol !== "") {
+    
       console.log("vo day A");
       dau += 1;
       number2 += id;
-
+ numberAndSign += id;
     
       displayNumber(number2);
-      numberAndSign += id;
+     
     } else {
       
+    
       console.log("vo dayB");
       dau += 1;
       number1 += id;
-           numberAndSign += id;
-       console.log("dau" +dau);
+        numberAndSign += id;
+    
      if(dau != 1)
      {
     displayNumber(number1);
